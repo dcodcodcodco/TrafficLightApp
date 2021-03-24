851b21bed36672cbe81706699379ea0117b6b477
dcodocdcodco <snoozelight0@gmail.com>
2021-03-24 23:03:16 +0300

    light without stack

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var currentLight = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = 75
+        redLightView.alpha = 0.3
+        
+        yellowLightView.layer.cornerRadius = 75
+        yellowLightView.alpha = 0.3
+        
+        greenLightView.layer.cornerRadius = 75
+        greenLightView.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 10
+        
     }
+    
+    
+    
+    @IBAction func startButtonPressed() {
+        currentLight += 1
 
-
+        switch currentLight {
+        case 1:
+            redLightView.alpha = 1.0
+            greenLightView.alpha = 0.3
+        case 2:
+            yellowLightView.alpha = 1.0
+            redLightView.alpha = 0.3
+        case 3:
+            greenLightView.alpha = 1.0
+            yellowLightView.alpha = 0.3
+        default:
+            break
+        }
+        
+        if currentLight == 3 {
+            currentLight = 0
+        }
+        
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+    }
 }
 
